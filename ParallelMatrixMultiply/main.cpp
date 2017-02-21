800040b1f38c17aae6315bc76873774d5fd4c9d6
Matthew McLean <mmclean@unbc.ca>
2017-02-20 19:37:37 -0800

    Set Up experiaments

--- ParallelMatrixMultiply/main.cpp
+++ ParallelMatrixMultiply/main.cpp
@@ -7,6 +7,18 @@
 #include <fstream>
 #include <iostream>
 
+//For Testing
+void printMatrix(unsigned long long *c, int m, int n) {
+	printf("\n");
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			printf("%d,", c[m*j + i]);
+		}
+		printf("\n");
+	}
+	printf("\n");
+}
+
 int test(unsigned int m, unsigned int n, unsigned int o, std::ofstream& output, int threads) {
 	//Ints to track timings
 	int begin_process; 
@@ -16,10 +28,9 @@ int test(unsigned int m, unsigned int n, unsigned int o, std::ofstream& output, 
 	unsigned long long *b = new unsigned long long[n * o]; //Allocate array A to heap
 	MatrixRandomFill(a, m, n); //Place random Data in Array A
 	MatrixRandomFill(b, n, o); //Place random Data in Array B
-
 	unsigned long long *openmp_columns = new unsigned long long[m*o];
 	memset(openmp_columns, 0, m * o * sizeof(long long));
-	output << "MPI_COLUMN, ";
+	output << "OpenMP-column, ";
 	output << threads;
 	output << ", ";
 	output << m;
@@ -37,7 +48,7 @@ int test(unsigned int m, unsigned int n, unsigned int o, std::ofstream& output, 
 
 	unsigned long long *openmp_elements = new unsigned long long[m*o];
 	memset(openmp_elements, 0, m * o * sizeof(long long));
-	output << "MPI_ELEMENT, ";
+	output << "OpenMP-Element, ";
 	output << threads;
 	output << ", ";
 	output << m;
@@ -56,7 +67,7 @@ int test(unsigned int m, unsigned int n, unsigned int o, std::ofstream& output, 
 	if (threads == 1) {
 		unsigned long long *cuda = new unsigned long long[m * o];
 		memset(cuda, 0, m * o * sizeof(long long));
-		output << "CUDA, 1920, ";
+		output << "cuda, 1920, ";
 		output << m;
 		output << ", ";
 		output << n;
@@ -80,80 +91,25 @@ int test(unsigned int m, unsigned int n, unsigned int o, std::ofstream& output, 
 
 int main()
 {
+	int threads[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 32, 64, 125, 256 };
 	std::ofstream file;
-	file.open("results10.csv");
+	file.open("resultsCUDA.csv");
 	file << "Process, Threads, M, N, O, Time(ms)\n";
-	int MAX_DIMENSION = 10;
-	for (int t = 0; t >= 0; t--) {
-		int tmp = pow(2, t);
-		printf("Threads: %d\n- - - - - - - - - - - - - - - - - - - - - -\n", tmp);
-		for (unsigned int m = MAX_DIMENSION; m >= 1; m--) {
-			printf("%d:", m);
-			for (unsigned int n = m; n >= 1; n--) {
-				printf(" %d", n);
-				for (unsigned int o = n; o >= 1; o--) {
-					printf(".");
+	int MAX_DIMENSION = 14;
+	for (int t = 0; t < sizeof(threads)/sizeof(threads[0]); t++) {
+		printf("Threads: %d\n- - - - - - - - - - - - - - - - - - - - - -\n", threads[t]);
+		for (unsigned int m = MAX_DIMENSION; m >= 7; m--) {
+			printf("%d:", m);	
 					for (unsigned int i = 0; i < 3; i++) {
-						test(pow(2, m), pow(2, m), pow(2, m), file, pow(2, t));
+						printf(".");
+						test(pow(2, m), pow(2, m), pow(2, m), file, threads[t]);
 					}
-				}
-			}
 			printf("\n");
 		}
 		printf("- - - - - - - - - - - - - - - - - - - - - -\n\n\n");
 	}
 	file << "End of test\n";
 	file.close();
-
-	std::ofstream file2;
-	file2.open("results11.csv");
-	file2 << "Process, Threads, M, N, O, Time(ms)\n";
-	MAX_DIMENSION = 11;
-	for (int t = 10; t >= 0; t--) {
-		int tmp = pow(2, t);
-		printf("Threads: %d\n- - - - - - - - - - - - - - - - - - - - - -\n", tmp);
-		for (unsigned int m = MAX_DIMENSION; m >= 1; m--) {
-			printf("%d:", m);
-			for (unsigned int n = m; n >= 1; n--) {
-				printf(" %d", n);
-				for (unsigned int o = n; o >= 1; o--) {
-					printf(".");
-					for (unsigned int i = 0; i < 3; i++) {
-						test(pow(2, m), pow(2, m), pow(2, m), file2, pow(2, t));
-					}
-				}
-			}
-			printf("\n");
-		}
-		printf("- - - - - - - - - - - - - - - - - - - - - -\n\n\n");
-	}
-	file2 << "End of test\n";
-	file2.close();
-
-	std::ofstream file3;
-	file3.open("results12.csv");
-	file3 << "Process, Threads, M, N, O, Time(ms)\n";
-	MAX_DIMENSION = 12;
-	for (int t = 10; t >= 0; t--) {
-		int tmp = pow(2, t);
-		printf("Threads: %d\n- - - - - - - - - - - - - - - - - - - - - -\n", tmp);
-		for (unsigned int m = MAX_DIMENSION; m >= 1; m--) {
-			printf("%d:", m);
-			for (unsigned int n = m; n >= 1; n--) {
-				printf(" %d", n);
-				for (unsigned int o = n; o >= 1; o--) {
-					printf(".");
-					for (unsigned int i = 0; i < 3; i++) {
-						test(pow(2, m), pow(2, m), pow(2, m), file3, pow(2, t));
-					}
-				}
-			}
-			printf("\n");
-		}
-		printf("- - - - - - - - - - - - - - - - - - - - - -\n\n\n");
-	}
-	file3 << "End of test\n";
-	file3.close();
 }
 
 
